594c84b70130f2dffe20c2f20aec04bf39bc9ded
Lukasz Laszko <lukaszlaszko@gmail.com>
2018-06-12 10:13:55 +0800

    issue-3: backtrace depth increased

--- tests/cxx/setup.cpp
+++ tests/cxx/setup.cpp
@@ -16,7 +16,7 @@ void handler(int sig)
     size_t size;
 
     // get void*'s for all entries on the stack
-    size = backtrace(array, 20);
+    size = backtrace(array, 50);
 
     // print out all the frames to stderr
     cerr << "Error: signal " << sig << ":" << endl;
